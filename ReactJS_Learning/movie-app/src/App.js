32cc4c4a081a07d34cc81e94164bd0775ccfed32
kkanyo <kkanyo33@gmail.com>
2023-02-12 22:46:02 +0900

    Add feature 'To Do List'

--- ReactJS_Learning/movie-app/src/App.js
+++ ReactJS_Learning/movie-app/src/App.js
@@ -1,7 +1,44 @@
+import { useState } from "react";
+
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+  console.log(toDos);
+  console.log(
+    toDos.map((item, index) => (
+      // 각 item을 리스트로 만들어 반환
+      <li key={index}>{item}</li>
+    ))
+  );
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ur>
+        {/* map: 각 element를 전달받아 새로운 값을 반환 */}
+        {toDos.map((item, index) => (
+          // 각 item을 리스트로 만들어 반환
+          <li key={index}>{item}</li>
+        ))}
+      </ur>
     </div>
   );
 }
