3c95f2c4419caece235438d57301813d3ece8c9e
kkanyo <kkanyo33@gmail.com>
2025-06-12 17:53:32 +0900

    feat: view article

--- springboot-practice/src/main/java/me/kkanyo/springbootpractice/domain/Article.java
+++ springboot-practice/src/main/java/me/kkanyo/springbootpractice/domain/Article.java
@@ -5,7 +5,13 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)  // 엔티티의 생성 및 수정 시간을 자동으로 감시하고 기록
 @Entity // 엔터티로 지정
 @Getter // 모든 필드에 대한 접근자 메서드 생성
 @NoArgsConstructor(access = AccessLevel.PROTECTED)  // 기본 생성자를 생성
@@ -32,4 +38,13 @@ public class Article {
         this.title = title;
         this.content = content;
     }
+    
+    @CreatedDate    // 엔티티가 생성될 때 생성 시간 저장
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    
+    @LastModifiedDate   // 엔티티가 수정될 때 수정 시간 저장
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
 }
