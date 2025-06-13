d4460c4544117bba108710e41aa0e06010946c44
kkanyo <kkanyo33@gmail.com>
2025-06-13 18:10:37 +0900

    feat: login/logout/signup with spring security

---
+++ springboot-practice/src/main/java/me/kkanyo/springbootpractice/controller/UserApiController.java
@@ -0,0 +1,34 @@
+package me.kkanyo.springbootpractice.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import me.kkanyo.springbootpractice.dto.AddUserRequest;
+import me.kkanyo.springbootpractice.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class UserApiController {
+
+    private final UserService userService;
+
+    @PostMapping("/user")
+    public String signup(AddUserRequest request) {
+        userService.save(request);
+
+        return "redirect:/login";   // 회원 가입이 완료된 이후에 로그인 페이지로 이동
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response,
+                SecurityContextHolder.getContext().getAuthentication());
+
+        return "redirect:/login";
+    }
+}
