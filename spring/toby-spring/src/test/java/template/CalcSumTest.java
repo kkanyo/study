f067ed8bb234141095dd9bbc3709b8a7ce336c8f
kkanyo <kkanyo33@gmail.com>
2025-08-06 16:54:27 +0900

    feat: template/callback pattern more

---
+++ spring/toby-spring/src/test/java/template/CalcSumTest.java
@@ -0,0 +1,36 @@
+package template;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import tobyspring.learningtest.template.Calculator;
+
+public class CalcSumTest {
+    Calculator calculator;
+    String numFilepath;
+
+    @BeforeEach
+    void setUp() {
+        this.calculator = new Calculator();
+        this.numFilepath = getClass().getResource("/numbers.txt").getPath();
+    }
+
+    @Test
+    public void sumOfNumbers() throws IOException {
+        assertThat(this.calculator.calcSum(this.numFilepath)).isEqualTo(10);
+    }
+
+    @Test
+    public void multipleOfNumbers() throws IOException {
+        assertThat(this.calculator.calcMultiple(this.numFilepath)).isEqualTo(24);
+    }
+
+    @Test
+    public void concatenateOfNumbers() throws IOException {
+        assertThat(this.calculator.concatenate(this.numFilepath)).isEqualTo("1234");
+    }
+}
