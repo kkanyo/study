b9d6a4189c4c1bfc4cebbd119f9f2668980757e2
kkanyo <kkanyo33@gmail.com>
2025-12-12 18:54:09 +0900

    Lambda Expression

--- java-practice/modern-java-in-action/src/chapter03/LambdaTest.java
+++ java-practice/modern-java-in-action/src/chapter03/LambdaTest.java
@@ -1,147 +1,97 @@
 package chapter03;
 
-import static java.util.Comparator.comparing;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
+import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 
-import chapter03.Fruit.Color;
-
+/**
+ * 람다 표현식은 메서드로 전달할 수 있는 익명 함수를 단순화한 것이라고 할 수 있다.
+ * 
+ * 람다 표현식의 특징
+ * 1. 익명: 보통의 메서드와 달리 이름이 없으므로 익명이라 표현한다.
+ * 2. 함수: 람다는 메서드처럼 특정 클래스에 종속되지 않는다.
+ * 메서드처럼 파라미터 리스트, 바디, 반환 형식, 가능한 예외 리스트를 포함한다.
+ * 3. 전달: 람다 표현식을 메서드 인수로 전달하거나 변수로 저장할 수 있다.
+ * 4. 간결성
+ * 
+ * 람다 표현식에는 return이 함축되어 있으므로 return 문을 명시적으로 사용하지 않아도 된다.
+ * 표현식 스타일(expression style): (parameters) -> expression
+ * 블록 스타일(block-style): (parameters) -> { statements; }
+ * 
+ * 
+ * 함수형 인터페이스는 정확히 하나의 추상 메서드를 지정하는 인터페이스다. (ex. Comparator, Runnable)
+ * 인터페이스는 디폴트 메서드(인터페이스의 메서드를 구현하지 않은 클래스를 고려해서
+ * 기본 구현을 제공하는 바디를 포함하는 메소드)를 포함할 수 없다.
+ * 많은 디폴트 메서드가 있더라도 `추상 메서드가 오직 하나면` 함수형 인터페이스다.
+ * 
+ * 람다 표현식으로 함수형 인터페이스의 추상 메서드 구현을 직접 전달할 수 있으므로
+ * 전체 표현식을 함수형 인터페이스의 인스턴스로 취급할 수 있다.
+ * (기술적으로 따지면 함수형 인터페이스를 구현한 클래스의 인스턴스)
+ * 
+ * 
+ * 함수형 인터페이스의 추상 메서드 시그니처(signature, 컴파일러나 인터프리터가 함수를 서로 구별하기 위해
+ * 사용하는 함수의 이름과 파타미터 목록 등 고유한 식별 정보)는 람다 표현식의 시그니처를 가리킨다.
+ * 람다 표현식의 시그니처를 서술하는 메서드를 함수 디스크립터(function desciptor)라고 부른다.
+ * 
+ * {@link @FunctionalInterface}
+ * 함수형 인터페이스임을 가리키는 어노테이션이다.
+ * 인터페이스를 선언했지만 실제로 함수형 인터페이스가 아니면 컴파일러가 에러를 발생시킨다.
+ * 
+ */
 public class LambdaTest {
     public static void main(String[] args) throws IOException {
-        String oneLine = processFile((BufferedReader br) -> br.readLine());
-        System.out.println(oneLine);
-        System.out.println();
-        
-        String twoLine = processFile((BufferedReader br) -> br.readLine() + "\n" + br.readLine());
-        System.out.println(twoLine);
-        System.out.println();
+        /**
+         * 중괄호는 필요 없다.
+         * 자바 언어 명세에서는 void를 반환하는 메소드 호출과 관련한 특별한 규칙을 정하고 있기 때문이다.
+         * 즉 한 개의 void 메소드 호출은 중괄호로 감쌀 필요가 없다.
+         */
+        process(() -> System.out.println("This is awesome!!\n"));
 
-        List<Integer> l = map(Arrays.asList("lambdas", "in", "action"),
-            (String s) -> s.length());
-        System.out.println(l);
-        System.out.println();
+        // Execute arround pattern
+        System.out.println("Execute arround pattern");
+        String oueLine = processFile((BufferedReader br) -> br.readLine());
+        System.out.printf("oueLine: %s\n", oueLine);
 
-        // method reference
-        System.out.println("메소드 참조");
-        List<String> str1 = Arrays.asList("a", "b", "A", "B");
-        List<String> str2 = Arrays.asList("a", "b", "A", "B");
-        str1.sort((s1, s2) -> s1.compareToIgnoreCase(s2));
-        str2.sort(String::compareToIgnoreCase);
-        System.out.println(str1);
-        System.out.println(str2);
-        System.out.println();
+        String twoLine = processFile((BufferedReader br) -> br.readLine() + br.readLine());
+        System.out.printf("twoLine: %s\n\n", twoLine);
 
-        // constructor reference
-        System.out.println("생성자 참조");
-        TriFunction<Color, Integer, Integer, Fruit> fruitFactory = Fruit::new;
-        Fruit apple = fruitFactory.apply(Color.GREEN, 155, 3);
-        apple.printFruit();
+        // Predicate
+        System.out.println("Predicate Test");
+        Predicate<String> nonEmptyStringPredicate = (String s) -> !s.isEmpty();
+        List<String> nonEmpty = UsingFunctionalInterface.filter(Arrays.asList("", "apple", "", "orange"),
+                nonEmptyStringPredicate);
+        System.out.println(nonEmpty + "\n");
+
+        // Consumer
+        System.out.println("Consumer Test");
+        UsingFunctionalInterface.forEach(Arrays.asList(1, 2, 3, 4, 5), (Integer i) -> System.out.println(i));
         System.out.println();
 
-        List<Fruit> inventory = new ArrayList<>();
-        inventory.add(new Fruit(Color.RED, 80, 1));
-        inventory.add(new Fruit(Color.RED, 155, 1));
-        inventory.add(new Fruit(Color.GREEN, 75, 1));
-        inventory.add(new Fruit(Color.GREEN, 80, 1));
+        // Function
+        System.out.println("Function Test");
+        List<Integer> l = UsingFunctionalInterface.map(Arrays.asList("lambdas", "in", "action"),
+                (String s) -> s.length());
+        System.out.println(l + "\n");
 
-        System.out.println("람다를 이용한 정렬");
-        // inventory.sort(new FruitComparator());
+        // Boxing and Unboxing
+        IntPredicate evenNumbers = (int i) -> i % 2 == 0;
+        evenNumbers.test(1000); // no boxing
 
-        // inventory.sort(new Comparator<Fruit>() {
-        //     public int compare(Fruit o1, Fruit o2) {
-        //         return o1.getWeight().compareTo(o2.getWeight());
-        //     }
-        // });
+        Predicate<Integer> oddNnumbers = (Integer i) -> i % 2 != 0;
+        oddNnumbers.test(1000); // boxing
+    }
 
-        // inventory.sort((o1, o2) -> o1.getWeight().compareTo(o2.getWeight()));
-
-        // inventory.sort(comparing(fruit -> fruit.getWeight()));
-
-        inventory.sort(comparing(Fruit::getWeight));
-
-        printFruits(inventory);
-
-        System.out.println("역정렬");
-        inventory.sort(comparing(Fruit::getWeight).reversed());
-
-        printFruits(inventory);
-
-
-        // connect Comperator
-        inventory.sort(comparing(Fruit::getWeight)
-            .thenComparing(Fruit::getColor));
-        
-        printFruits(inventory);
-
-        // combine Predicate
-        Predicate<Fruit> redFruit = (fruit) -> fruit.getColor() == Color.RED;
-        Predicate<Fruit> notRedFruit = redFruit.negate();
-
-        List<Fruit> notRedFruits = filter(inventory, notRedFruit);
-        printFruits(notRedFruits);
-
-        Predicate<Fruit> redAndHeavyFruit = redFruit.and(fruit -> fruit.getWeight() > 150);
-        List<Fruit> redAndHeavyFruits = filter(inventory, redAndHeavyFruit);
-        printFruits(redAndHeavyFruits);
-
-        Predicate<Fruit> redAndHeavyFruitOrGreen = redAndHeavyFruit
-            .or(fruit -> Color.GREEN.equals(fruit.getColor()));
-        List<Fruit> redAndHeavyFruitsOrGreen = filter(inventory, redAndHeavyFruitOrGreen);
-        printFruits(redAndHeavyFruitsOrGreen);
-
-        // combine Function
-        Function<Integer, Integer> f = x -> x + 1;
-        Function<Integer, Integer> g = x -> x * 2;
-        Function<Integer, Integer> h = f.andThen(g);    // g(f(x))
-        int result = h.apply(1);
-        System.out.println(result);
-        System.out.println();
-
-        h = f.compose(g);   // f(g(x))
-        result = h.apply(1);
-        System.out.println(result);
-        System.out.println();
-
+    public static void process(Runnable r) {
+        r.run();
     }
 
     public static String processFile(BufferedReaderProcessor p) throws IOException {
-        try (BufferedReader br = 
-                new BufferedReader(new FileReader("./resources/data.txt"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("./resources/data.txt"))) {
             return p.process(br);
         }
-    }
-
-    public static <T, R> List<R> map(List<T> list, Function<T, R> f) {
-        List<R> result = new ArrayList<>();
-
-        for (T t: list) {
-            result.add(f.apply(t));
-        }
-
-        return result;
-    }
-
-    public static <T> List<T> filter(List<T> list, Predicate<T> p) {
-        List<T> result = new ArrayList<>();
-
-        for (T e: list) {
-            if (p.test(e)) {
-                result.add(e);
-            }
-        }
-
-        return result;
-    }
-
-    private static void printFruits(List<Fruit> list) {
-        list.forEach(e -> e.printFruit());
-        System.out.println();
     }
 }
